d47c85a4b92ce8fea3eaa7766605f49b2401ba75
chenyc1995 <chenyc1995@live.com>
2022-05-17 18:38:31 +0800

    Update Bili_Auto_Regions.js

--- Surge/JS/Bili_Auto_Regions.js
+++ Surge/JS/Bili_Auto_Regions.js
@@ -76,7 +76,7 @@ let $ = nobyda();
 let run = EnvInfo();
 
 async function SwitchRegion(play) {
-	const Group = $.read('BiliArea_Policy') || '📺 DomesticMedia'; //Your blibli policy group name.
+	const Group = $.read('BiliArea_Policy') || 'StreamingSE'; //Your blibli policy group name.
 	const CN = $.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
 	const TW = $.read('BiliArea_TW') || '台湾子策略'; //Your Taiwan sub-policy name.
 	const HK = $.read('BiliArea_HK') || '香港子策略'; //Your HongKong sub-policy name.
